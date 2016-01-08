71f778264ff4061883ccf859264185c39db4e615
Andrew Innes <andrew.c12@gmail.com>
2016-01-08 00:24:41 -0500

    Added comments to files

--- bananapisdimg.sh
+++ bananapisdimg.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#this concatenates the banana pi specific firmware with the more generic armhf the debian installer
 wget http://ftp.uk.debian.org/debian/dists/stretch/main/installer-armhf/current/images/netboot/SD-card-images/firmware.BananaPi.img.gz
 wget http://ftp.uk.debian.org/debian/dists/stretch/main/installer-armhf/current/images/netboot/SD-card-images/partition.img.gz
 zcat firmware.BananaPi.img.gz partition.img.gz > firmware.BananaPi+partition.img
